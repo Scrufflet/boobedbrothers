e8d750e5f6e9d880058ea048859fe3a6bc74040a
Corey Urquhart <CoreyU49@gmail.com>
2013-12-16 22:29:08 +0000

    Added to index.php and created header stylesheet.
    Added font link.
    Added Jquery links.
    Added CSS links.

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
 		<title>
 			There's no space for us, Barney!
 		</title>
+
+		<link rel="stylesheet" href="css/main.php" type="text/css">
+		<link rel="SHORTCUT ICON" href="img/icons/home.png" type="image/x-icon">
+  		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+  		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
 	</head>
 	
 	<body>

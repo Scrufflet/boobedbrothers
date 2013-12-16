43492ca1b5be95e51b9ed764c8f095601a05f287
Corey Urquhart <CoreyU49@gmail.com>
2013-12-16 22:52:12 +0000

    asd
    asd

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
 
-	include('php/variables.php/')
+	session_start();
+	
 ?>
 
 <html>
@@ -9,14 +9,6 @@
 		<title>
 			There's no space for us, Barney!
 		</title>
-
-		<link rel="stylesheet" href="css/main.php" type="text/css">
-		<link rel="SHORTCUT ICON" href="img/icons/home.png" type="image/x-icon">
-  		<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-  		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-
 	</head>
 	
 	<body>
